853fef1862100dc418d9dc2eeab045c7ff403a68
Jared Fuller <jaredshane@gmail.com>
2017-03-05 19:37:59 -0600

    added some more colored lines for the flag exercise as a beginning test

--- 01-foundations/third-party-modules/flag.js
+++ 01-foundations/third-party-modules/flag.js
@@ -7,9 +7,14 @@
 const bold = string => `\u001b[1m${string}\u001b[22m`
 const green = string => `\u001b[32m${string}\u001b[39m`
 const red = string => `\u001b[31m${string}\u001b[39m`
-console.log(`It is ${red('easier')} to use ${green('functions for')} ${green(bold('ANSI'))} codes!`)
+// console.log(`It is ${red('easier')} to use ${green('functions for')} ${green(bold('ANSI'))} codes!`)
 
 
 const CHALK = require('chalk')
 
-console.log(CHALK.bgBlue('                  ') + CHALK.bgRed('                                        '))
+console.log(CHALK.bgBlue('  * * * * * * *   ') + CHALK.bgRed('                                        '))
+console.log(CHALK.bgBlue('   * * * * * *    ') + CHALK.bgWhite('                                        '))
+console.log(CHALK.bgBlue('  * * * * * * *   ') + CHALK.bgRed('                                        '))
+console.log(CHALK.bgBlue('   * * * * * *    ') + CHALK.bgWhite('                                        '))
+console.log(CHALK.bgBlue('  * * * * * * *   ') + CHALK.bgRed('                                        '))
+  
