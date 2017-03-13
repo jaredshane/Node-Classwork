b3a9aa4f6b3f30b9f5dda6d1f8fb05fccff56777
Jared Fuller <jaredshane@gmail.com>
2017-03-13 16:24:35 -0500

    added calc directory with some basic testing for a calculator app

---
+++ 01-foundations/calc-tdd/test/add-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { assert: {isNumber}  } = require("chai");
+const add = require('../lib/add');
+
+describe('add', () => {
+  it('should return a number', () => {
+    isNumber(add())
+  })
+})
