85437e3e3f20e8dcffd4c60b1eef322d61aaf946
Jared Fuller <jaredshane@gmail.com>
2017-03-14 12:42:52 -0500

    removed empty string and uncommented out the actual code

--- 01-foundations/17-modularity-diceroll/lib/parse-args.js
+++ 01-foundations/17-modularity-diceroll/lib/parse-args.js
@@ -2,7 +2,7 @@
 
 module.exports = (argsArr) => {
   let dieNums = {};
-  dieNums.count = '' //argsArr[1] ? argsArr[0] : 1;
-  dieNums.sides = '' //argsArr[1] ? argsArr[1] : argsArr[0] || 6;
+  dieNums.count =  argsArr[1] ? argsArr[0] : 1;
+  dieNums.sides =  argsArr[1] ? argsArr[1] : argsArr[0] || 6;
   return dieNums;
 };
