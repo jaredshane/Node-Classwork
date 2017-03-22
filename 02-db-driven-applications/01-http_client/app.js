c69544397f25ecaa0bf765513ba36367844402a1
Jared Fuller <jaredshane@gmail.com>
2017-03-22 16:32:46 -0500

    added directory for the first exercise from 02-db-driven-applications

---
+++ 02-db-driven-applications/01-http_client/app.js
@@ -0,0 +1,42 @@
+const { get } = require('http')
+
+const { argv: [,, arg]} = process
+
+
+
+get(`http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters={"Normalized":false,"NumberOfDays":365,"DataPeriod":"Day","Elements":[{"Symbol":"${arg}","Type":"price","Params":["c"]}]}`, (res) => {
+  // console.log(res)
+
+  const statusCode = res.statusCode
+  const contentType = res.headers['content-type']
+  let error = null
+  // console.log(res.headers)
+  if (statusCode !== 200) {
+    error = new Error(`Request failed.\nStatus Code: ${statusCode}`)
+  }
+  else if (!/^text\/javascript/.test(contentType)) {
+    error = new Error(`Invalid content-type.\nExpected text/javascript but received ${contentType}`)
+  }
+
+  if (error) {
+    console.log(error.message)
+    res.resume();
+    return;
+  }
+
+
+  let dataReturned = '';
+  res.on('data', (buff) => {
+    // console.log('buff', buff)
+    dataReturned += buff.toString()
+    console.log('dataReturned', dataReturned)
+    // console.log(body)
+  })
+  res.on('end', () => {
+    let { Elements: [{ DataSeries: { close: { values }} }] } = JSON.parse(dataReturned)
+    // console.log(values)
+    let sum = values.reduce((a, b) => a + b, 0);
+    sum = sum / 365
+    console.log(sum.toFixed(2));
+  })
+})
