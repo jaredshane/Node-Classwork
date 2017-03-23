e1d1e72b3add5e223cddbeacdcbf9d71b0ca0c6c
Jared Fuller <jaredshane@gmail.com>
2017-03-23 09:53:28 -0500

    created a server and it reads the index.html file

--- 02-db-driven-applications/02-http-server/server01.js
+++ 02-db-driven-applications/02-http-server/server01.js
@@ -5,6 +5,7 @@ const server = createServer()
 server.on('request', (req, res) => {
 
   readFile('index.html', (err, buff) => {
+    console.log(`Request received for ${req.url}`)
     if (err) {
       console.log('err', err)
       return res.end()
